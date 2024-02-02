2777060569c55a2a2ee0b7a85d592b24998d9f71
JeongSoyoung <sspkl124@gmail.com>
2024-02-02 15:04:27 +0900

    UPDATED

---
+++ project.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import pandas as pd
+
+SERVICE_KEY = "6a4e556e6b6568663433776e41656b"
+
+df=None
+for i in range(301,375):
+    URL=f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{1+(i-1)*1000}/{i*1000}/'
+    print(URL)
+    req= requests.get(URL)
+    content=req.json()
+    result=pd.DataFrame(content['tbLnOpendataRtmsV']['row'])
+    df=pd.concat([df,result])
+df=df.reset_index(drop=True)
+df.to_csv('.sample.csv',index=False)
+
