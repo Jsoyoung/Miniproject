770a0577d16fcaf876c42664f59b771f7b43da19
JeongSoyoung <sspkl124@gmail.com>
2024-02-05 22:34:54 +0900

    UPDATED

--- transaction_counts.py
+++ transaction_counts.py
@@ -9,14 +9,35 @@ import plotly.express as px
 
 def main():
     df = load_data()
-    st.subheader('지역구 용도별 거래건수 막대그래프')
+    st.subheader("서울시 자치구별 거래건수")
     st.markdown(''':green[서울시 부동산 실거래가 정보 기준] :green[**(접수 기간 : 2020~2023년도)**]''')
+    # 자치구별 거래건수 계산
+    district_transaction_counts = df.groupby('SGG_NM').size().reset_index(name='거래건수')
+    # 거래건수 내림차순 정렬
+    district_transaction_counts = district_transaction_counts.sort_values(by='거래건수', ascending=False).reset_index(drop=True)
+
+    # 막대그래프 출력
+    fig_district = px.bar(district_transaction_counts, x='SGG_NM', y='거래건수', color='SGG_NM', color_discrete_sequence=px.colors.qualitative.Set3)
+    fig_district.update_layout(xaxis_title='자치구', legend_title='자치구')
+    st.plotly_chart(fig_district)
+
+    # 가장 거래건수가 많은 자치구
+    max_transaction_district = district_transaction_counts.loc[district_transaction_counts['거래건수'].idxmax()]
+    
+    # 결과 출력
+    result_text = f"*자치구명 : {max_transaction_district['SGG_NM']}    *거래건수 : {max_transaction_district['거래건수']} 건"
+    st.markdown(':star: :green[**거래건수가 가장 많은 자치구**]')
+    st.text(result_text)
+
+    st.markdown('---')
+
+    st.subheader('자치구 용도별 거래건수 막대그래프')
 
     # 선택한 지역구 및 용도에 따른 데이터 필터링
-    selected_sgg = st.selectbox('구를 선택하세요.', df['SGG_NM'].unique())
+    selected_sgg = st.sidebar.selectbox('구를 선택하세요.', df['SGG_NM'].unique())
     filtered_df = df[df['SGG_NM'] == selected_sgg]
 
-    selected_house_type = st.selectbox('용도를 선택하세요.', df['HOUSE_TYPE'].unique())
+    selected_house_type = st.sidebar.selectbox('용도를 선택하세요.', df['HOUSE_TYPE'].unique())
     filtered_df = filtered_df[filtered_df['HOUSE_TYPE'] == selected_house_type]
 
     # 거래건수 계산
@@ -45,6 +66,8 @@ def main():
     st.markdown("---")
     st.markdown(''':star: :green[**거래량 3순위**]''')
     st.dataframe(transaction_counts.head(3))
+
+
 
 
 
